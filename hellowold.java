90fefe8a3e835e2fecac3e95f2dacdb0155b5437
Ubuntu <cloud_user@ubuntu005.443dw2ckvuaulg24v5vqxt0tce.tx.internal.cloudapp.net>
2021-03-24 11:24:59 +0000

    edited latest

--- hellowold.java
+++ hellowold.java
@@ -2,7 +2,7 @@ class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World Mani2!"); 
+        System.out.println("Hello, World Mani3!"); 
 
     }
 
